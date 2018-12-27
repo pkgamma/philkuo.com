f5be125a20ce8b085acaa1203ecb123aefd2b725
pkgamma <pkgamma@gmail.com>
2018-12-27 07:11:04 -0600

    changed .eleventy.js to add what files to be included when build

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.setTemplateFormats("html,liquid,njk,css,js,eot,ttf,woff,svg,jpg,png,cur");
+  eleventyConfig.setTemplateFormats("html, liquid, njk, css, js, eot, ttf, woff, svg, jpg, gif, png, cur");
   eleventyConfig.addPassthroughCopy("_redirects");
 };
