f97e1458948d13c4f417a66fef166e9f3648f08e
Philip Kuo <pkgamma@gmail.com>
2021-01-13 02:58:50 +0800

    added footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,28 +7,39 @@ export default function Layout({ children }) {
   return (
     <div>
 
-      <div className={styles.sticky_navbar}>
-        <div className={styles.navbar}>
-          <div className={styles.left}>
-            <ul>
-              <li className={styles.name}>Philip Kuo</li>
-              <li><a className={styles.current} href="">About</a></li>
-              <li><a href="">Work</a></li>
-            </ul>
-          </div>
-          <div className={styles.spacer}></div>
-          <div className={styles.right}>
-            <ul>
-              <li><a className={styles.current} href="">EN</a></li>
-              <li><a href="">中</a></li>
-            </ul>
+      <div className={styles.page_container}>
+        <div className={styles.sticky_navbar}>
+          <div className={styles.navbar}>
+            <div className={styles.left}>
+              <ul>
+                <li className={styles.name}>Philip Kuo</li>
+                <li><a className={styles.current} href="">About</a></li>
+                <li><a href="">Work</a></li>
+              </ul>
+            </div>
+            <div className={styles.spacer}></div>
+            <div className={styles.right}>
+              <ul>
+                <li><a className={styles.current} href="">EN</a></li>
+                <li><a href="">中</a></li>
+              </ul>
+            </div>
           </div>
         </div>
+
+        <div className={styles.main}>
+          {children}
+        </div>
+
+        <div className={styles.footer_container}>
+        <div className={styles.footer}>
+          <p>Copyright © 2021 Philip Kuo. View the <a href="http://archive.philkuo.com">Project Archive</a>.</p>
+        </div>
+      </div>
       </div>
 
-      <div className={styles.main}>
-        {children}
-      </div>
+      
+
 
     </div>
   )
