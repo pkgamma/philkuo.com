56dca2c530d38f7d25341131eb93e787600ec365
Philip Kuo <pkgamma@gmail.com>
2021-01-13 00:08:36 +0800

    title bar basics done

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,13 +5,29 @@ import "normalize.css"
 
 export default function Layout({ children }) {
   return (
+
     <div>
 
-      <div className={styles.navbar}>
-
-        Content
-
+      <div className={styles.sticky_navbar}>
+        <div className={styles.navbar}>
+          <div className={styles.left}>
+            <ul>
+              <li className={styles.name}>Philip Kuo</li>
+              <li><a className={styles.current} href="">About</a></li>
+              <li><a href="">Work</a></li>
+            </ul>
+          </div>
+          <div className={styles.spacer}></div>
+          <div className={styles.right}>
+            <ul>
+              <li><a className={styles.current} href="">EN</a></li>
+              <li><a href="">中</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
+
+
 
       <div className={styles.main}>
         {children}
