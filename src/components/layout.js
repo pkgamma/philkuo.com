dacf98025ed8d54a59f303e9f728f05aed491efc
Philip Kuo <pkgamma@gmail.com>
2021-01-13 23:27:16 +0800

    created link and hover style for main copy text

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
-import styles from "./layout.module.scss"
 import "normalize.css"
+import styles from "./layout.module.scss"
 import Navbar from "./navbar"
 import Footer from "./footer"
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react"
-
 import Layout from "../components/layout"
-
 import styles from "./index.module.scss"
-
-import { faGithub, faLinkedin, faMedium, faMediumM } from '@fortawesome/free-brands-svg-icons'
+import PhilipKuoPic from "../img/philip_kuo.jpg"
+import { faGithub, faLinkedin, faMediumM } from '@fortawesome/free-brands-svg-icons'
 import { faFile, faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -15,7 +13,7 @@ export default function Home() {
       <div className={styles.container}>
 
         <div className={styles.pic}>
-          <img alt="Philip Kuo" src="https://d33wubrfki0l68.cloudfront.net/f889430202cebb38797416bf08da875a98ff4585/a607f/img/philip_kuo.jpg" />
+          <img alt="Philip Kuo" src={PhilipKuoPic} />
         </div>
 
         <h1>
@@ -24,16 +22,16 @@ export default function Home() {
         </h1>
 
         <p>Philip Kuo is a software engineer with a passion for human-centered design. He designs and develops intuitive products with great attention to detail.</p>
-        <p>He currently works at Moshi, and previously at Caterpillar and DeepWalk Research. He founded PKGamma Digital Agency, organized a TEDx, and helped put together HackIllinois and the RP Tech Conference.</p>
+        <p>He currently works at Moshi, and previously at <a href="https://www.builtinchicago.org/company/cat-digital">Caterpillar</a> and DeepWalk Research. He founded PKGamma Digital Agency, organized a TEDx, and helped put together HackIllinois and the RP Tech Conference.</p>
         <p>In the meantime, he's pursuing a Computer Science degree from the University of Illinois at Urbana Champaign. At school, he’s developing at Hack4Impact to empower non-profits that are doing meaningful work to communities.</p>
 
         <div className={styles.social_section}>
           <ul>
-            <li><a className={styles.resume} href="http://www.google.com"><FontAwesomeIcon icon={faFile} className={styles.icon} />Resume</a></li>
-            <li><a href="http://www.google.com"><FontAwesomeIcon icon={faEnvelope} className={styles.icon} />Email</a></li>
-            <li><a href="http://www.google.com"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} />LinkedIn</a></li>
-            <li><a href="http://www.google.com"><FontAwesomeIcon icon={faGithub} className={styles.icon} />Github</a></li>
-            <li><a href="http://www.google.com"><FontAwesomeIcon icon={faMediumM} className={styles.icon} />Medium</a></li>
+            <li><a className={styles.resume} href="http://philkuo.com/resume"><FontAwesomeIcon icon={faFile} className={styles.icon} />Resume</a></li>
+            <li><a href="mailto:pk@philkuo.com"><FontAwesomeIcon icon={faEnvelope} className={styles.icon} />Email</a></li>
+            <li><a href="https://linkedin.com/in/pkgamma/"><FontAwesomeIcon icon={faLinkedin} className={styles.icon} />LinkedIn</a></li>
+            <li><a href="https://github.com/pkgamma"><FontAwesomeIcon icon={faGithub} className={styles.icon} />Github</a></li>
+            <li><a href="https://medium.com/@pkgamma"><FontAwesomeIcon icon={faMediumM} className={styles.icon} />Medium</a></li>
           </ul>
         </div>
 
