ada9b265325bd09888ad5f0b0c3889aae57ec80d
Philip Kuo <pkgamma@gmail.com>
2021-01-21 12:45:32 +0800

    added link to Work items

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import styles from "./work.module.scss"
+import { Link } from "gatsby"
 import BG1 from "../img/work/1.jpg"
 import BG2 from "../img/work/2.jpg"
 import BG3 from "../img/work/3.jpg"
@@ -15,41 +16,42 @@ export default function Work() {
       </div>
 
       <div className={styles.grid_container}>
-        <div className={styles.item_container}>
+        
+        <Link to="/work/one" className={styles.item_container}>
           <div className={styles.pic} style={{ backgroundImage: "url(" + BG1 + ")" }}>
           </div>
           <div className={styles.description_container}>
             <div className={styles.title}>Item Title One</div>
             <div className={styles.description}>Lorem ipsum dolor sit amet</div>
           </div>
-        </div>
+        </Link>
 
-        <div className={styles.item_container}>
+        <Link className={styles.item_container}>
           <div className={styles.pic} style={{ backgroundImage: "url(" + BG2 + ")" }}>
           </div>
           <div className={styles.description_container}>
             <div className={styles.title}>Item Title Two</div>
             <div className={styles.description}>Lorem ipsum dolor sit amet</div>
           </div>
-        </div>
+        </Link>
 
-        <div className={styles.item_container}>
+        <Link className={styles.item_container}>
           <div className={styles.pic} style={{ backgroundImage: "url(" + BG3 + ")" }}>
           </div>
           <div className={styles.description_container}>
             <div className={styles.title}>Item Title Three</div>
             <div className={styles.description}>Lorem ipsum dolor sit amet</div>
           </div>
-        </div>
+        </Link>
 
-        <div className={styles.item_container}>
+        <Link className={styles.item_container}>
           <div className={styles.pic} style={{ backgroundImage: "url(" + BG4 + ")" }}>
           </div>
           <div className={styles.description_container}>
             <div className={styles.title}>Item Title Four</div>
             <div className={styles.description}>Lorem ipsum dolor sit amet</div>
           </div>
-        </div>
+        </Link>
 
       </div>
 
