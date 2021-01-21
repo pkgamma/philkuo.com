b103596de2c6aa56ec179b9859e9c65eb959e05a
Philip Kuo <pkgamma@gmail.com>
2021-01-21 15:08:56 +0800

    added back button and responsive body widths for Work item pages

--- src/pages/work/one.js
+++ src/pages/work/one.js
@@ -2,6 +2,8 @@ import React from "react"
 import Layout from "../../components/layout"
 import styles from "./work_post.module.scss"
 import { Link } from "gatsby"
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import BG1 from "../../img/work/1.jpg"
 
 export default function One() {
@@ -9,7 +11,10 @@ export default function One() {
     <Layout>
 
       <div className={styles.title_container}>
-        <div className={styles.back}>Back</div>
+        <Link to="/work" className={styles.back}>
+          <FontAwesomeIcon icon={faChevronLeft} className={styles.icon} />
+          Back
+        </Link>
         <h1>The Munual Case Study</h1>
       </div>
 
@@ -27,7 +32,7 @@ export default function One() {
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ornare vestibulum odio, ac bibendum justo sodales at. Pellentesque pretium sapien ornare gravida sodales. Cras id ante eget arcu euismod blandit. Etiam feugiat, nunc molestie commodo molestie, elit nulla mollis nisi, hendrerit facilisis dolor est vitae nunc. Pellentesque sodales at massa non facilisis. Mauris facilisis justo lacus, at dictum turpis porta eu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam porta eleifend mi at sollicitudin.</p>
 
       </article>
-      
+
     </Layout>
   )
 }
