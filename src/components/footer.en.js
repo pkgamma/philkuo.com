22ea36cbd5855f1a6c639ff66a5660db9077f30c
Philip Kuo <pkgamma@gmail.com>
2021-02-09 13:02:23 +0800

    improved navbar and footer language redirect logic

--- src/components/footer.en.js
+++ src/components/footer.en.js
@@ -1,8 +1,11 @@
 import { Link } from "gatsby"
 import React from "react"
 import styles from "./footer.module.scss"
+import { useLocation } from "@reach/router"
 
 export default function Footer() {
+  const { pathname } = useLocation()
+
   return (
     <div className={styles.sticky_footer}>
       <div className={styles.footer}>
@@ -12,7 +15,7 @@ export default function Footer() {
             <span><a target="_blank" rel="noreferrer" href="http://archive.philkuo.com">Project Archive</a></span>
             <span><Link to="/privacy">Privacy</Link></span>
             <span className={styles.language}>
-              <span><Link to="/zh">中文</Link></span>
+              <span><Link to={`${"/zh"}${pathname}`}>中文</Link></span>
             </span>
           </span>
         </p>
--- src/components/footer.zh.js
+++ src/components/footer.zh.js
@@ -1,8 +1,11 @@
 import { Link } from "gatsby"
 import React from "react"
 import styles from "./footer.module.scss"
+import { useLocation } from "@reach/router"
 
 export default function Footer() {
+  const { pathname } = useLocation()
+
   return (
     <div className={styles.sticky_footer}>
       <div className={styles.footer}>
@@ -12,7 +15,7 @@ export default function Footer() {
             <span><a target="_blank" rel="noreferrer" href="http://archive.philkuo.com">過去專案封存</a></span>
             <span><Link to="/zh/privacy">隱私權政策</Link></span>
             <span className={styles.language}>
-              <span><Link to="/">English</Link></span>
+              <span><Link to={`${"/."}${pathname.replace("/zh", "")}`}>English</Link></span>
             </span>
           </span>
         </p>
