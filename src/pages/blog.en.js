c9bbd63d447f2bab5ab6958f5b37a36777dc3c20
Philip Kuo <pkgamma@gmail.com>
2021-04-15 12:27:43 +0800

    added links for more blog posts originally from Medium

--- src/pages/blog.en.js
+++ src/pages/blog.en.js
@@ -5,6 +5,8 @@ import BG2 from "./work/img/A02.jpg"
 import BG3 from "./work/img/B01.jpg"
 import BG4 from "./work/img/B02.jpg"
 import img_minimalist_workplace from "./img/minimalist_workplace.jpg"
+import img_disney_posters from "./img/disney_posters.jpg"
+import img_builtworlds from "./img/builtworlds.jpg"
 import SEO from '../components/seo';
 import ItemCard from "../components/item-card.en"
 import Title from "../components/title.en"
@@ -15,13 +17,15 @@ export default function Blog() {
       <SEO title={"Blog - Philip Kuo"} />
       <Title title="Blog"/>
       <div className="grid_container">
-        <ItemCard link="https://medium.com/swlh/my-digital-workspace-1790a0b0420c" image={img_minimalist_workplace} title="My Minimalist Digital Workspace" description="Sharing the Beautiful, Powerful Apps I Use Everyday"/>
-        <ItemCard link={null} image={BG1} title="HackIllinois 2020" description="Making the Largest Hackathon in Midwest Happen"/>
-        <ItemCard link={null} image={BG1} title="RP Tech Conference 2019" description="Marketing for a Nationwide Presence"/>
-        <ItemCard link={null} image={BG1} title="Interconnect CRM" description="Attempt of Building a Business from Scratch"/>
-        <ItemCard link={null} image={BG1} title="TEDx" description="Curating an Extraordinary TEDx Event"/>
-        <ItemCard link={null} image={BG1} title="Menu3" description="Building an Elegant Brand Identity"/>
-        <ItemCard link={null} image={BG1} title="The Munual" description="The Manual to Model United Nations"/>
+        <ItemCard link="https://medium.com/swlh/my-digital-workspace-1790a0b0420c" image={img_minimalist_workplace} title="My Minimalist Digital Workspace" description="Sharing the beautiful, powerful apps I use everyday."/>
+        <ItemCard link="https://medium.com/@pkgamma/the-story-of-creating-the-perfect-disney-themed-dorm-room-b9d8da9c1f44" image={img_disney_posters} title="Preserving Vintage Posters from Disney Parks" description="Digitally restoring the stunning Disney attraction posters."/>
+        <ItemCard link="https://medium.com/@pkgamma/builtworlds-hackathon-2019-a180fddf1024" image={img_builtworlds} title="BuiltWorlds Hackathon 2019" description="Winning the hackathon with a great team."/>
+        <ItemCard link={null} image={BG1} title="HackIllinois 2020" description="Making the largest hackathon in midwest happen."/>
+        <ItemCard link={null} image={BG1} title="RP Tech Conference 2019" description="Marketing for a nationwide presence."/>
+        <ItemCard link={null} image={BG1} title="Interconnect CRM" description="Attempt of building a business from scratch."/>
+        <ItemCard link={null} image={BG1} title="TEDx" description="Curating an extraordinary TEDx event."/>
+        <ItemCard link={null} image={BG1} title="Menu3" description="Building an elegant brand identity."/>
+        <ItemCard link={null} image={BG1} title="The Munual" description="The manual to Model United Nations."/>
       </div>
     </Layout>
   )
--- src/pages/blog.zh.js
+++ src/pages/blog.zh.js
@@ -5,6 +5,8 @@ import BG2 from "./work/img/A02.jpg"
 import BG3 from "./work/img/B01.jpg"
 import BG4 from "./work/img/B02.jpg"
 import img_minimalist_workplace from "./img/minimalist_workplace.jpg"
+import img_disney_posters from "./img/disney_posters.jpg"
+import img_builtworlds from "./img/builtworlds.jpg"
 import SEO from '../components/seo';
 import ItemCard from "../components/item-card.zh"
 import Title from "../components/title.zh"
@@ -16,6 +18,8 @@ export default function Blog() {
       <Title title="經驗"/>
       <div className="grid_container">
         <ItemCard link="https://medium.com/swlh/my-digital-workspace-1790a0b0420c" image={img_minimalist_workplace} title="我的極簡數位工作站" description="分享我最常使用的幾個軟體 (英文)"/>
+        <ItemCard link="https://medium.com/@pkgamma/the-story-of-creating-the-perfect-disney-themed-dorm-room-b9d8da9c1f44" image={img_disney_posters} title="數位修復迪士尼樂園之中古海報" description="利用科技蒐集保留這些極具歷史的藝術海報 (英文)"/>
+        <ItemCard link="https://medium.com/@pkgamma/builtworlds-hackathon-2019-a180fddf1024" image={img_builtworlds} title="BuiltWorlds 黑客松 2019" description="與完美團隊在芝加哥贏得這活動的故事 (英文)"/>
         <ItemCard link={null} image={BG1} title="HackIllinois 黑客松 2020" description="舉辦美國中部最具規模的開發競賽活動"/>
         <ItemCard link={null} image={BG1} title="RP 科技論壇 2019" description="為全美國透過多管道行銷這個活動"/>
         <ItemCard link={null} image={BG1} title="Interconnect 客戶關係管理程式" description="與朋友打造這個科技新創公司"/>
