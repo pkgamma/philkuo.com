eaf5bc6663ede5076e98d826bdaf69ea07ccbd4c
Philip Kuo <pkgamma@gmail.com>
2021-04-16 22:50:55 +0800

    added iPhone Wireframe Template page

--- src/pages/blog.en.js
+++ src/pages/blog.en.js
@@ -7,6 +7,7 @@ import BG4 from "./work/img/B02.jpg"
 import img_minimalist_workplace from "./img/minimalist_workplace.jpg"
 import img_disney_posters from "./img/disney_posters.jpg"
 import img_builtworlds from "./img/builtworlds.jpg"
+import img_iphone_wireframes from "./img/iphone_wireframes.jpg"
 import SEO from '../components/seo';
 import ItemCard from "../components/item-card.en"
 import Title from "../components/title.en"
@@ -17,6 +18,7 @@ export default function Blog() {
       <SEO title={"Blog - Philip Kuo"} />
       <Title title="Blog"/>
       <div className="grid_container">
+        <ItemCard link={null} image={img_iphone_wireframes} title="iPhone 12 Pro Wireframe Templates" description="Made for the community, free to download."/>
         <ItemCard link="https://medium.com/swlh/my-digital-workspace-1790a0b0420c" image={img_minimalist_workplace} title="My Minimalist Digital Workspace" description="Sharing the beautiful, powerful apps I use everyday."/>
         <ItemCard link="https://medium.com/@pkgamma/the-story-of-creating-the-perfect-disney-themed-dorm-room-b9d8da9c1f44" image={img_disney_posters} title="Preserving Vintage Posters from Disney Parks" description="Digitally restoring the stunning Disney attraction posters."/>
         <ItemCard link="https://medium.com/@pkgamma/builtworlds-hackathon-2019-a180fddf1024" image={img_builtworlds} title="BuiltWorlds Hackathon 2019" description="Winning the hackathon with a great team."/>
--- src/pages/blog.zh.js
+++ src/pages/blog.zh.js
@@ -7,6 +7,7 @@ import BG4 from "./work/img/B02.jpg"
 import img_minimalist_workplace from "./img/minimalist_workplace.jpg"
 import img_disney_posters from "./img/disney_posters.jpg"
 import img_builtworlds from "./img/builtworlds.jpg"
+import img_iphone_wireframes from "./img/iphone_wireframes.jpg"
 import SEO from '../components/seo';
 import ItemCard from "../components/item-card.zh"
 import Title from "../components/title.zh"
@@ -17,6 +18,7 @@ export default function Blog() {
       <SEO zh title={"經驗 - 郭力銘 Philip Kuo"} />
       <Title title="經驗"/>
       <div className="grid_container">
+        <ItemCard link={null} image={img_iphone_wireframes} title="iPhone 12 Pro 線搞圖模板" description="提供給設計與工程師社群的免費資源"/>
         <ItemCard link="https://medium.com/swlh/my-digital-workspace-1790a0b0420c" image={img_minimalist_workplace} title="我的極簡數位工作站" description="分享我最常使用的幾個軟體 (英文)"/>
         <ItemCard link="https://medium.com/@pkgamma/the-story-of-creating-the-perfect-disney-themed-dorm-room-b9d8da9c1f44" image={img_disney_posters} title="數位修復迪士尼樂園之中古海報" description="利用科技蒐集保留這些極具歷史的藝術海報 (英文)"/>
         <ItemCard link="https://medium.com/@pkgamma/builtworlds-hackathon-2019-a180fddf1024" image={img_builtworlds} title="BuiltWorlds 黑客松 2019" description="與完美團隊在芝加哥贏得這活動的故事 (英文)"/>
